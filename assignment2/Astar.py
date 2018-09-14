a1d020f68ed842735b1a6d04d989b1b98eef5173
Aksel Danielsen <akselsd@stud.ntnu.no>
2018-09-14 19:09:04 +0200

    Assignment 2

---
+++ assignment2/Astar.py
@@ -0,0 +1,98 @@
+import board
+import math
+
+#Propegate changes when a cheaper path to a node is found
+def update_existing_node(new_parent, new_path, existing_node):
+	if existing_node.g > new_path + existing_node.cost:
+		existing_node.g = new_path + existing_node.cost
+		existing_node.parent = new_parent
+		for kid in existing_node.kids:
+			update_existing_node(existing_node, existing_node.g, kid)
+
+
+# Get the minimum cost node from the open set
+def extract_min(nodes):
+	min_score = math.inf
+	min_index = 0
+	for i, node in enumerate(nodes):
+		if min_score > node.g + node.h:
+			min_score = node.g + node.h
+			min_index = i
+
+	return nodes.pop(min_index)
+
+# Backtrack from the goal node, add each node to the path list
+def build_path(node):
+	path = []
+	while(node.parent.parent != None):
+		node = node.parent
+		path.append((node.x, node.y))
+	return path
+
+# Main algorithm
+def a_star(board):
+
+	#Initialize
+	closed = []
+	open_nodes = [board.get_start_node()]
+
+	while open_nodes:
+
+		# Get next node to explore
+		current = extract_min(open_nodes)
+
+		# If it is a solution, we are finished
+		if board.is_goal_node(current):
+			return build_path(current)
+
+		# Get adjecent nodes
+		neighbours = board.get_neighbour_nodes(current)
+
+		# Marked the current node as closed
+		closed.append(current)
+
+		# Iterate over the newly found nodes
+		for node in neighbours:
+
+			# If it is already explored, ignore it
+			if node in closed:
+				continue
+
+			# If it is already found, check if this is a better path
+			if node in open_nodes:
+				update_existing_node(current, current.g, open_nodes[open_nodes.index(node)])
+				continue
+
+			# Add new node to open set
+			node.g = node.cost + current.g
+			node.h = board.get_distance_to_goal(node)
+			open_nodes.append(node)
+
+	return None
+
+def main():
+	txtfiles = [
+#		"board-1-0.txt",
+#		"boards/board-1-1.txt",
+#		"boards/board-1-2.txt",
+		"boards/board-1-3.txt",
+#		"boards/board-1-4.txt",
+		"boards/board-2-1.txt",
+		"boards/board-2-2.txt",
+		"boards/board-2-3.txt",
+		"boards/board-2-4.txt"
+	]
+	for txt in txtfiles:
+		b = board.Board(txt)
+		solution = a_star(b)
+		b.print_board([])
+		print()
+		b.print_board(solution)
+		print()
+
+if __name__ == "__main__":
+	for i in range(100):
+		print(
+			"\033[" + str(i) + "m " + str(i) + " \033[0m")
+	main()
+	
---
+++ assignment2/board.py
@@ -0,0 +1,166 @@
+import math
+
+WALL = "#"
+START = "A"
+GOAL = "B"
+
+CHAR_TO_COST = {
+	"w": 100,
+	"m": 50,
+	"f": 10,
+	"g": 5,
+	"r": 1,
+	".": 1,
+	"A": 0,
+	"B": 0
+}
+
+CHAR_TO_COLOR = {
+	".": "35",
+	"r": "35",
+	"w": "34",
+	"m": "92",
+	"f": "32",
+	"g": "33",
+	"o": "30",
+	"A": "31",
+	"B": "47",
+	"#": "37"
+}
+print_colors = True
+
+class Node:
+
+	# Initialize new node
+	def __init__(self, x, y, cost, parent):
+		self.parent = parent
+		self.x = x
+		self.y = y
+		self.cost = cost
+		self.g = math.inf
+		self.h = math.inf
+		self.kids = []
+
+	def __hash__(self):
+		return hash((self.x, self.y))
+
+	def __cmp__(self, other):
+		return cmp(self.g+self.h, other.g + other.h)
+
+	def __eq__(self, other):
+		if (other == None):
+			return False
+		return (self.x == other.x and self.y == other.y)
+
+	def __str__(self):
+		return "(" + str(self.x) + " ," + str(self.y) + ")"
+
+	def __repr__(self):
+		return self.__str__()
+
+
+class Board:
+
+	# Load the into an array of strings
+	def _load_board_from_file(self, txtfile):
+		with open(txtfile) as f:
+			for line in f.readlines():
+				self._board.append(line.strip())
+
+		self._size_y = len(self._board)
+		self._size_x = len(self._board[0])
+		for row in self._board:
+			if len(row) != self._size_x:
+				print("Error loading board, board size not rectangular")
+				print("Expected", self._size_x, "got", len(row),". Row: ", row)
+				exit()
+
+		for y in range(self._size_y):
+			for x in range(self._size_x):
+				if self._board[y][x] == START:
+					self._start = (x, y)
+				if self._board[y][x] == GOAL:
+					self._goal = (x, y)
+
+	# Decide if a node is a wall
+	# Edges is treated as walls
+	def _is_wall(self, x, y):
+		if x < 0 or x >= self._size_x:
+			return True
+
+		if y < 0 or y >= self._size_y:
+			return True
+
+		return self._board[y][x] == WALL
+
+	# Checks for walls in all 4 directions
+	def _get_possible_directions(self, node):
+		dirs = []
+		for direction in [(+1, 0), (-1, 0), (0, +1), (0, -1)]:
+			if not self._is_wall(
+				direction[0] + node.x,
+				direction[1] + node.y):
+				dirs.append(direction)
+		return dirs
+
+	# Create new nodes from all possible neighbours
+	def get_neighbour_nodes(self, node):
+		dirs = self._get_possible_directions(node)
+		neighbours = []
+		for d in dirs:
+			x = node.x + d[0]
+			y = node.y + d[1]
+			neighbours.append(Node(
+				x,
+				y,
+				CHAR_TO_COST[self._board[y][x]],
+				node))
+
+		node.kids.extend(neighbours)
+		return neighbours
+
+	# Return manhatten distance to goal
+	def get_distance_to_goal(self, node):
+		return abs(node.x - self._goal[0]) + abs(node.y - self._goal[1])
+
+	# Return startnode represented by A
+	def get_start_node(self):
+		n = Node(
+			self._start[0],
+			self._start[1],
+			0,
+			None)
+		n.g = 0
+		n.h = self.get_distance_to_goal(n)
+		return n
+
+	# Checks if a given node is the goal node
+	def is_goal_node(self, node):
+		return (node.x, node.y) == self._goal
+
+	#Prints the board
+	# @param solution is an array of nodes that makes up the solution path
+	def print_board(self, solution):
+		for y in range(self._size_y):
+			for x in range(self._size_x):
+				if (x, y) in solution:
+					char = "o"
+				else:
+					char = self._board[y][x]
+				if print_colors:
+					char = char_to_color(char)
+				print(char, end ="")
+			print()
+
+	def __init__(self, txtfile):
+		self._board = []
+		self._size_x = 0
+		self._size_y = 0
+		self._goal = None
+		self._start = None
+		self._load_board_from_file(txtfile)
+
+def char_to_color(char):
+	fg = CHAR_TO_COLOR[char]
+	bg = str(int(CHAR_TO_COLOR[char]) + 10)
+	return "\033[" + fg + ";" + bg + "m" + char + "\033[0m"
